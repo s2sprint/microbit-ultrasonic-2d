afc8d1d4f593f53aed433745e98d821dd132f6d2
s2sprint <69420623+s2sprint@users.noreply.github.com>
2020-09-23 09:50:34 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 let rad = 0
 let len = 0
-let x = 0
 let y = 0
+let x = 0
 basic.forever(function () {
     rad = input.compassHeading() / 360 * 6.28318
     len = grove.measureInCentimeters(DigitalPin.P0)
-    x = len * Math.cos(rad)
-    y = len * Math.sin(rad)
+    y = len * Math.cos(rad)
+    x = len * Math.sin(rad)
     serial.writeLine("x:" + x)
     serial.writeLine("y:" + y)
     basic.pause(100)
